10d4dd3988fc238acf19f57bcae7226d89aaf332
LitleStuart <vanyamukhori@icloud.com>
2024-03-08 17:16:17 +0500

    rewrite to Rusian lang

--- src/app/help.rs
+++ src/app/help.rs
@@ -1,8 +1,10 @@
-use teloxide::{dptree::case, prelude::*};
+use teloxide::{
+  dptree::case, prelude::*, utils::command::BotCommands,
+};
 
 use crate::{
-    app, bot_commands::MenuCommand, common::Result,
-    utils::filter_message,
+  app, bot_commands::MenuCommand, common::Result,
+  utils::filter_message,
 };
 
 use super::UpdateHandler;
@@ -10,25 +12,25 @@ use super::UpdateHandler;
 pub struct Plugin;
 
 impl app::Plugin for Plugin {
-    fn update_handler(&self) -> UpdateHandler {
-        filter_message()
-            .filter_command::<MenuCommand>()
-            .branch(case![MenuCommand::Help].endpoint(send_help))
-    }
+  fn update_handler(&self) -> UpdateHandler {
+    filter_message()
+      .filter_command::<MenuCommand>()
+      .branch(case![MenuCommand::Help].endpoint(send_help))
+  }
 }
 
 async fn send_help(bot: Bot, chat_id: ChatId) -> Result<()> {
-    const HELP_MESSAGE: &str = "
-Use this bot for:
-- save and track your sugar measurements
-- get reminders for an insulin injections
-- see your average amount of insulin
+  let help_message = format!("
+Этот бот имеет следующие возможности:
+- сохранение и просмотр ваших показаний уровня сахара
+- отправка напоминаний о необходимости измерения сахара и/или инъекции инсулина
+- просмотр среднего количества инсулина за период времени.
 
-List of commands:
-  /help - print this message
-  /sugar_level - send your last sugar measurement
-  /insulin_injection - send your last insulin injection
-";
-    bot.send_message(chat_id, HELP_MESSAGE).await?;
-    Ok(())
+Функциональность постепенно увеличивается, бот находится в активной разработке. 
+
+Список доступных команд:
+{}", MenuCommand::descriptions());
+
+  bot.send_message(chat_id, help_message).await?;
+  Ok(())
 }
